93d02f376f99c9eafc05877eba900a21d23d3e9b
Karl0077 <62748470+Karl0077@users.noreply.github.com>
2020-06-01 01:17:36 +0800

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -13,14 +13,14 @@ from PIL import Image
 import numpy as np
 
 # для конфигурации gpu
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
+#from tensorflow.compat.v1 import ConfigProto
+#from tensorflow.compat.v1 import InteractiveSession
 
 # настраиваем работу с GPU, для CPU эта часть не нужна
-config = ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.7
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
+#config = ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.7
+#config.gpu_options.allow_growth = True
+#session = InteractiveSession(config=config)
 
 height = 224
 width = 224
